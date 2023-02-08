f7ddf7cbff087be9dc5d2b8a898c228bd02ada93
t101804 <t101804@protonmail.com>
2023-02-08 19:16:57 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mouuff/go-rocket-update-example
+
+go 1.15
+
+require github.com/mouuff/go-rocket-update v1.5.2
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"runtime"
+	"sync"
+
+	"github.com/mouuff/go-rocket-update/pkg/provider"
+	"github.com/mouuff/go-rocket-update/pkg/updater"
+)
+
+func main() {
+	u := &updater.Updater{
+		Provider: &provider.Github{
+			RepositoryURL: "github.com/mouuff/go-rocket-update-example",
+			ArchiveName:   fmt.Sprintf("binaries_%s.zip", runtime.GOOS),
+		},
+		ExecutableName: fmt.Sprintf("go-rocket-update-example_%s_%s", runtime.GOOS, runtime.GOARCH),
+		Version:        "v2.5.0", // You can change this value to trigger an update
+	}
+
+	versionFlag := false
+	flag.BoolVar(&versionFlag, "version", false, "prints the version and exit")
+	flag.Parse()
+
+	if versionFlag {
+		// we use this flag to verify the installation for this example:
+		// https://github.com/mouuff/go-rocket-update/blob/master/examples/github-rollback/main.go
+		fmt.Println(u.Version)
+		return
+	}
+
+	log.Println("Current version: " + u.Version)
+	log.Println("Looking for updates...")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	// For the example we run the update in the background
+	// but you could directly call u.Update()
+	var updateStatus updater.UpdateStatus
+	var updateErr error
+	go func() {
+		updateStatus, updateErr = u.Update()
+		wg.Done()
+	}()
+
+	// Here you can execute your program
+	wg.Wait() // Waiting for the update process to finish before exiting
+	if updateErr != nil {
+		log.Println(updateErr)
+	}
+	if updateStatus == updater.Updated {
+		log.Println("Updated!")
+	}
+}
